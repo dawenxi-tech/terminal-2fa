aac386b17ed9a15d757b7716d9e856a0c279c1f1
dawenxi <dawenxi.tech@gmail.com>
2024-11-14 22:41:20 +0800

    add time remain

--- 2fa/main.go
+++ 2fa/main.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jedib0t/go-pretty/v6/table"
@@ -42,15 +43,26 @@ func display2FA() {
 		slog.With(slog.String("err", err.Error())).Error("error to read configure")
 		os.Exit(1)
 	}
+	for {
+		str := render2FA(objs)
+		_, _ = os.Stdout.WriteString(str)
+		time.Sleep(time.Second)
+		lineCount := len(strings.Split(str, "\n"))
+		clearStr := strings.Repeat("\033[K\033[A", lineCount-1)
+		_, _ = os.Stdout.WriteString(clearStr)
+	}
+}
+
+func render2FA(objs []Entry) string {
 	tw := table.NewWriter()
 	tw.SetTitle("2FA")
 	tw.SetIndexColumn(1)
-	tw.AppendHeader(table.Row{"#", "Name\t\t", "Code\t\t", "Time\t\t"})
+	tw.AppendHeader(table.Row{"#", "Name\t\t", "Code\t\t", "Remain\t"})
 	now := time.Now().Unix()
 	for i, obj := range objs {
 		code := gotp.NewDefaultTOTP(obj.Seed).At(now)
-		tw.AppendRow(table.Row{strconv.Itoa(i + 1), obj.Name + "\t", code + "\t", "\t10s"})
+		tw.AppendRow(table.Row{strconv.Itoa(i + 1), " " + obj.Name + "\t", " " + code + "\t", fmt.Sprintf(" %ds", 30-time.Now().Second()%30)})
 	}
 	tw.SetCaption("Use -config to manager codes.")
-	fmt.Println(tw.Render())
+	return "\n" + tw.Render()
 }
